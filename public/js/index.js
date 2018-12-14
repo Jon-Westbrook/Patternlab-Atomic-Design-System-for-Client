14b2ac609ab92ccd6dbca9c1476131ff33ce7ca2
Jonathon <jwestbrook@vsapartners.com>
2018-12-14 16:30:12 -0500

    close button in menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,11 @@
-
 $(document).ready(function() {
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
+  });
+  $('#dismiss, .overlay').on('click', function() {
+    // hide sidebar
+    $('#sidebar').removeClass('active');
+    // hide overlay
+    $('.overlay').removeClass('active');
   });
 });
---
+++ source/_patterns/02-Modules/M35-Navigation-Top.md
@@ -0,0 +1,5 @@
+---
+title: M2-Navigation-Top
+---
+
+The top hamburger menu that appears at smaller breakpoints.
