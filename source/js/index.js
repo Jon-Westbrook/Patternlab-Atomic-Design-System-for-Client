319aa79badbeafd4e4ed80f90e1a5b1cc4884360
Jonathon Westbrook <jwestbrook@vsapartners.com>
2019-01-09 16:46:26 -0500

    updates validation styles

--- source/js/index.js
+++ source/js/index.js
@@ -9,4 +9,27 @@ $(document).ready(function() {
     // hide overlay
     $('.overlay').removeClass('active');
   });
+
+  // Listen for Submit Events and Validate Form Field
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+
+        if (form.find(".form-control").hasClass(":invalid")) {
+          console.log("Somethin is wrong")
+          // form.querySelector('.primary-2').style.color = ("green");
+        }
+
+      }, false);
+    });
+  }, false);
+
 });
